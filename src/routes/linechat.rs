fbd39943acfb96db6f9f0e9388852e9a58318da0
Wendell Liu <cuk.bas@gmail.com>
2020-03-06 22:21:06 +0800

    parse from message body

--- src/routes/linechat.rs
+++ src/routes/linechat.rs
@@ -1,3 +1,16 @@
-pub async fn handler() {
+use serde::{Deserialize, Serialize};
+use serde_json::from_slice;
+use hyper::{body, Body, Request, Response};
 
+#[derive(Debug, Serialize, Deserialize)]
+struct ReqBody {
+    destination: String
+}
+
+pub async fn handler(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let _req_body = _req.into_body();
+    let bytes = body::to_bytes(_req_body).await.unwrap();
+    let s: ReqBody = from_slice(&bytes).unwrap();
+    println!("{:?}", s);
+    Ok(Response::new("line message".into()))
 }
--- src/routes/root.rs
+++ src/routes/root.rs
@@ -1,4 +1,6 @@
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Method, Request, Response, StatusCode};
+use crate::routes::linechat;
+
 
 async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     Ok(Response::new("Hello, World".into()))
@@ -7,6 +9,7 @@ async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, hyper::Error
 pub async fn handler(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => hello_world(req).await,
+        (&Method::POST, "/linechat") => linechat::handler(req).await,
         _ => {
             let mut not_found = Response::default();
             *not_found.status_mut() = StatusCode::NOT_FOUND;
