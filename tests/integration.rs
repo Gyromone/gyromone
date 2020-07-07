390b497c040b48e853566b517888c64423c0b0fc
Wendell Liu <cuk.bas@gmail.com>
2020-07-07 23:24:06 +0800

    bind uri

--- tests/integration.rs
+++ tests/integration.rs
@@ -10,11 +10,8 @@ mod tests {
 
     #[test]
     fn healthy_check() {
-        let addr = String::from("localhost");
-        let root_uri = format!("http://{}", addr);
-        let uri = format!("{}/{}", root_uri, "");
-
         let test_server = support::start_test_server();
+        let uri = support::bind_uri("");
 
         let response = test_server.client().get(uri).perform().unwrap();
         assert_eq!(response.status(), StatusCode::NO_CONTENT);
--- tests/support/mod.rs
+++ tests/support/mod.rs
@@ -2,6 +2,14 @@ use gyromone::routes;
 
 use gotham::test::TestServer;
 
+pub fn bind_uri(endpoint: &'static str) -> String {
+    let addr = String::from("localhost");
+    let root_uri = format!("http://{}", addr);
+    let uri = format!("{}/{}", root_uri, endpoint);
+
+    uri
+}
+
 pub fn start_test_server() -> TestServer {
     let router = routes::router();
     TestServer::new(router).unwrap()
